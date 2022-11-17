8770c2f605f479bf4662987e5aca108c62103abb
IvanVargass <iivanvargas@hotmail.com>
2022-11-17 00:14:10 -0500

    Slider listos

---
+++ js/swiper.js
@@ -0,0 +1,85 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        520: {
+         slidesPerView: 1,
+         spaceBetween: 20,
+        },
+        575: {
+         slidesPerView: 2,
+         spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        1400:{
+            slidesPerView: 3,
+            spaceBetween: 10,
+        }
+      },
+  });
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        520: {
+         slidesPerView: 3,
+         spaceBetween: 20,
+        },
+        575: {
+         slidesPerView: 3,
+         spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 10,
+        },
+        1400:{
+            slidesPerView: 6,
+            spaceBetween: 10,
+        }
+      },
+  });
+
+
